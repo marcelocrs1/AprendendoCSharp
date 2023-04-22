a90116f3c7f0d46b006b9a8442cb60438a148b95
Marcelo Santos <marcelocrs@gmail.com>
2023-04-22 14:40:05 +0100

    Aula Criação de variaveis finalizada

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Varáveis");
+        int idade;
+        idade = 31;
+
+        idade ++;
+
+        Console.WriteLine("Minha idade é: " + idade);
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
