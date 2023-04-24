d4810361956717a70d95916cb49597d54d11e6dc
Marcelo Santos <marcelocrs@gmail.com>
2023-04-24 15:20:10 +0100

    Aula 3 e 4 finalizadas

---
+++ 3-CriandoVariaveisBoleanas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Boleanas ou Ponto Flutuante");
+
+        double salario;
+
+        salario = 3000.10;
+
+        double idade;
+
+        idade = 7.0 / 5;
+
+        Console.WriteLine("Meu Salário é: " + salario);
+        Console.WriteLine(idade);
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões e outros tipos");
+
+        double salario;
+        salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro;
+
+        salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+
+        long x;
+        x = 2000000000000;
+        Console.WriteLine(x);
+
+
+        short y; //Valores até 16bits
+        y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.62f; // Não é tão precisa 
+        Console.WriteLine(altura);
+
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
