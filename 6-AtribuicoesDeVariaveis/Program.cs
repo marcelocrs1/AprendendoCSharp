d903e7395982a8763b560360431965d23a2ec907
Marcelo Santos <marcelocrs@gmail.com>
2023-04-24 15:59:17 +0100

    Aula 6 Finalizada

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Atribuições de Variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
