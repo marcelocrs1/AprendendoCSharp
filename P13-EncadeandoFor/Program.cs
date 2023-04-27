845ce6764c1d76e1746040d27700caefce615b65
Marcelo Santos <marcelocrs@gmail.com>
2023-04-27 23:16:53 +0100

    Aula 13 condluida

---
+++ P13-EncadeandoFor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando For");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        //com break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        //sem break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas <= contadorLinhas; contadorColunas++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
