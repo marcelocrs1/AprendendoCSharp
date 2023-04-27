9efcd92a42c9a213a1f0b356c92e9d660b0475af
Marcelo Santos <marcelocrs@gmail.com>
2023-04-27 13:01:49 +0100

    Aula 8 concluida

---
+++ 8-CondicionaisBoleanas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        bool grupo = true;
+
+        if (idadeJoao >= 18 || acompanhado) // Podemos usar o || = Ou    && = E
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+
+            Console.WriteLine("Não Pode Entrar!");
+
+
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
