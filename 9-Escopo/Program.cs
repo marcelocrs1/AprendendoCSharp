52c8abd9a87598d502ab3f533f5c53dd8d68e72a
Marcelo Santos <marcelocrs@gmail.com>
2023-04-27 13:14:33 +0100

    Aula 9 Concluida

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+
+        string textoAdicional;
+
+        if (acompanhado == true)
+        {
+            textoAdicional = "João Está acompanhado!";
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado!";
+        }
+
+        if (idadeJoao >= 18 || acompanhado) // Podemos usar o || = Ou    && = E
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Não Pode Entrar!");
+
+
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
