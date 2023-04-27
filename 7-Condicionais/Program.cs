194d3e727b9d417b5b8e41ae5cadc83a5c8eed0e
Marcelo Santos <marcelocrs@gmail.com>
2023-04-27 12:54:10 +0100

    Aula Condicionais concluida

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode Entrar!");
+        }
+        else
+        {
+            if (quantidadePessoas > 1)
+            {
+                Console.WriteLine("Ele Está acompanhado pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Não Pode Entrar!");
+            }
+
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
