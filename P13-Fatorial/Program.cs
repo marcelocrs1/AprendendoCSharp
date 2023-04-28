65f298fc722c856792efc56440883b73754156f1
Marcelo Santos <marcelocrs@gmail.com>
2023-04-28 15:40:09 +0100

    Curso 1 C# Finalizado

---
+++ P13-Fatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Exercicio Multiplos de 3");
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            fatorial *= i;   
+            Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+        }
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
---
+++ P13-MultiplosDe3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Exercicio Multiplos de 3");
+
+
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+
+        // Ou 
+
+        for (int i = 3; i < 100; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
+
+
